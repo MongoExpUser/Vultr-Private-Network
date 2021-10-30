0b6e3ed34d1f8ffddcd29dbbf60b1d51c5b8f0fc
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2021-10-30 14:52:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,21 +3,21 @@ terraform {
 
   required_providers {
     vultr = {
-      source = "vultr/vultr"
-      version = "2.4.2"
+      source          = "vultr/vultr"
+      version         = "2.4.2"
     }
   }
     
-  backend "local" {
-   
+  backend "local" { 
+  
   }
 }
 
 
 # 1. create vultr database private network (db prn)
 resource "vultr_private_network" "private_network" {
-    description = var.private_network_description
-    region = var.vultr_region_name
-    v4_subnet  = var.v4_subnet
-    v4_subnet_mask = var.v4_subnet_mask
+    description       = var.private_network_description
+    region            = var.vultr_region_name
+    v4_subnet         = var.v4_subnet
+    v4_subnet_mask    = var.v4_subnet_mask
 }

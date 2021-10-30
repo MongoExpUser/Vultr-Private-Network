4f9dcbdd642886e3346232926cd5804a00f63ca4
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2021-10-30 10:52:43 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "vultr_private_network_attributes" {
   description = "key-value pair attributes of the created private network "
-  value = vultr_private_network.db_private_network
+  value = vultr_private_network.private_network
 }
